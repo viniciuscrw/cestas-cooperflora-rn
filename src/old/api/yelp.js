7b12e99fbf84cd3a68c74176186bad6767b7c740
Vinicius Costa <viniciuscregaco@gmail.com>
2021-04-24 16:39:32 -0300

    Removendo arquivos utilizados para referencia

--- src/old/api/yelp.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.yelp.com/v3/businesses',
-  headers: {
-    Authorization:
-      'Bearer PhdKswAR_TvxgUkzvRv3FHJ45kT1mLIO9pVilksgyyL2Hqnwv9ew4vKM4WOgKOfeQ4F-Z8QbSLsc-IosdlijjMfntZi7HqWEzPUV7i7xGAgFAPXO4R5vKZZ-kq7eXnYx',
-  },
-});
