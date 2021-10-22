d24dd8d890be44432103a618b3cd9883f0c3ef1a
André Luís Bordignon <albordignon@gmail.com>
2021-10-22 16:38:46 -0300

    Ajustando o firebase config.

--- src/constants/FirebaseConfig.js
+++ src/constants/FirebaseConfig.js
@@ -1,23 +1,23 @@
-// export default {
-//   FirebaseConfig: {
-//     apiKey: 'AIzaSyAOyjOVcCZnHRzATllhAh8x48sg8kMFphY',
-//     authDomain: 'cestas-cooperflora-dev-1375c.firebaseapp.com',
-//     databaseURL: 'https://cestas-cooperflora-dev-1375c.firebaseio.com',
-//     projectId: 'cestas-cooperflora-dev-1375c',
-//     storageBucket: 'cestas-cooperflora-dev-1375c.appspot.com',
-//     messagingSenderId: '68980862546',
-//     appId: '1:68980862546:web:69a7625891fc43eb3a798b',
-//   },
-// };
-
 export default {
   FirebaseConfig: {
-    apiKey: 'AIzaSyDd6zz4XqA7CIeL4AhcrKlclh8PK2EpzIA',
-    authDomain: 'cestas-cooperflora-dev2.firebaseapp.com',
-    databaseURL: 'https://cestas-cooperflora-dev2-default-rtdb.firebaseio.com/',
-    projectId: 'cestas-cooperflora-dev2',
-    storageBucket: 'cestas-cooperflora-dev2.appspot.com',
-    messagingSenderId: '109563362615',
-    appId: '1:109563362615:web:25896a03f004d33970aeb2',
+    apiKey: 'AIzaSyAOyjOVcCZnHRzATllhAh8x48sg8kMFphY',
+    authDomain: 'cestas-cooperflora-dev-1375c.firebaseapp.com',
+    databaseURL: 'https://cestas-cooperflora-dev-1375c.firebaseio.com',
+    projectId: 'cestas-cooperflora-dev-1375c',
+    storageBucket: 'cestas-cooperflora-dev-1375c.appspot.com',
+    messagingSenderId: '68980862546',
+    appId: '1:68980862546:web:69a7625891fc43eb3a798b',
   },
 };
+
+// export default {
+//   FirebaseConfig: {
+//     apiKey: 'AIzaSyDd6zz4XqA7CIeL4AhcrKlclh8PK2EpzIA',
+//     authDomain: 'cestas-cooperflora-dev2.firebaseapp.com',
+//     databaseURL: 'https://cestas-cooperflora-dev2-default-rtdb.firebaseio.com/',
+//     projectId: 'cestas-cooperflora-dev2',
+//     storageBucket: 'cestas-cooperflora-dev2.appspot.com',
+//     messagingSenderId: '109563362615',
+//     appId: '1:109563362615:web:25896a03f004d33970aeb2',
+//   },
+// };
